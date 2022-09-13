cf8b24518c98c30def1442358d51c084fdfb2b26
Joaquin Fernandez <jofernan@c3r7s5.42urduliz.com>
2022-09-13 18:23:27 +0200

    couldnt end CPP01 today bcs of filestreams

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,33 +1,54 @@
 #include <iostream>
 #include <fstream>
+
+void	writeOutstream(std::string s1, std::string s2, std::ifstream ifs, std::ofstream  ofs){
+	return ;
+}
 //program that takes as argv a filename and 2 strings (not empty)
 //then it opens. filename.replace and replaces every occurrence of s1 with s2
 //std::string member fuctions are allowed except for replace
 //argv[1] == filename
 //argv[2] == s1
 //argv[3] == s2
-//bucle while en el que 
 int main(int argc, char **argv){
 	if (argc != 4)
 		return 1;
+
 	std::string	filename = argv[1];
 	std::string	s1 = argv[2];
 	std::string	s2 = argv[3];
 	std::string	temp;
+	std::string	str;
+
 	if (s1.empty() || s2.empty())
 		return 1;
+
+	for (std::string::size_type i = 0; i < filename.length(); ++i)
+		filename[i] = std::toupper(filename[i]);
 	filename += ".replace";
+
 	std::ifstream	ifs(argv[1]);
 	std::ofstream	ofs(filename);
-	while(1){
-		ifs >> temp;
-		ofs << temp;
-		//if  (!temp.compare(s1))
-		//	ofs << s2;
-		//else
-		//	ofs << temp;
-		std::cout << temp << std::endl;
-	}
+
+	writeOutstream(s1, s2, ifs, ofs);
+	/*char c;
+	size_t i = 0;
+	std::string aux;
+	while(ifs.get(c)){
+		if (c == s1[i] && i < s1.length()){
+			aux += c;
+			i++;
+		}
+		else if (i == s1.length()){
+			ofs << s2;
+			ofs << c;
+			i = 0;
+		}
+		else{
+			ofs << c;
+			i = 0;
+		}
+	}*/
 	ofs.close();
 	ifs.close();
 	return 0;
